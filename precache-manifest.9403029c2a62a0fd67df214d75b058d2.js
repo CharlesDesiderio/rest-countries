2adab8e1b3db7d04ed863b021b13c7545b324cf1
Charles Desiderio <cjdesi@gmail.com>
2019-08-18 21:05:02 +0800

    Updates

---
+++ precache-manifest.9403029c2a62a0fd67df214d75b058d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c59ddbe62f8298fe6a065012801141",
+    "url": "/rest-countries/index.html"
+  },
+  {
+    "revision": "0024e2f18367ac4a414e",
+    "url": "/rest-countries/static/css/main.3469a8db.chunk.css"
+  },
+  {
+    "revision": "80d9d1eb000f3f0793cd",
+    "url": "/rest-countries/static/js/2.17b8ff39.chunk.js"
+  },
+  {
+    "revision": "0024e2f18367ac4a414e",
+    "url": "/rest-countries/static/js/main.14ebcd2e.chunk.js"
+  },
+  {
+    "revision": "80e0ccf2d2bb083ad009",
+    "url": "/rest-countries/static/js/runtime~main.0acb3de5.js"
+  }
+]);
